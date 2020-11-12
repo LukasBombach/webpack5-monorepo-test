edb360893654b47a3f609513a58a8f6b481cdfb3
Lukas Bombach <lukas.bombach@googlemail.com>
2020-11-12 19:12:15 +0100

    played with the dll plugin

--- packages/monopack/library.webpack.config.js
+++ packages/monopack/library.webpack.config.js
@@ -1,29 +1,29 @@
 const { resolve } = require("path");
-const { BannerPlugin, DllReferencePlugin } = require("webpack");
+const webpack = require("webpack");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: {
+    library: ["react", "ink"],
+  },
   target: "node",
   externals: [nodeExternals()],
   context: __dirname,
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "babel-loader",
-      },
-    ],
-  },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: [".js"],
     modules: [__dirname, "node_modules"],
   },
   output: {
-    filename: "index.js",
-    path: resolve(__dirname, "dist"),
+    filename: "[name].dll.js",
+    path: resolve(__dirname, "dist/library"),
+    library: "[name]",
   },
-  plugins: [new BannerPlugin({ banner: "#!/usr/bin/env node", raw: true })],
+  plugins: [
+    new webpack.DllPlugin({
+      name: "[name]",
+      path: resolve(__dirname, "dist/library/[name].json"),
+    }),
+  ],
   optimization: {
     minimize: false,
     removeAvailableModules: false,
